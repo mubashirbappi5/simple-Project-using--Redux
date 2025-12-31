fd7673c194c31529f7394e6647c82b2b51760a4f
mubashirbappi5 <mubashirbappi@gmail.com>
2025-12-31 21:15:43 +0600

    theme set

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,15 @@ import { RouterProvider } from 'react-router'
 import router from './routes/index.tsx'
 import { Provider } from 'react-redux'
 import { store } from './Redux/store.ts'
+import { ThemeProvider } from './Providers/theme-provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+  <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
      <Provider store={store}>
     <RouterProvider router={router}/>
     </Provider>
+    </ThemeProvider>
     
   </StrictMode>,
 )
